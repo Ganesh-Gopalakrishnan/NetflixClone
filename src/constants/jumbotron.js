9a7dee0731df85366e1699f4a186772641df99b8
Ganesh-Gopalakrishnan <88496221+Ganesh-Gopalakrishnan@users.noreply.github.com>
2021-11-29 01:59:10 +0530

    home unused removed

---
+++ src/constants/jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Jumbotron from "../components/jumbotron";
+
+export function JumbotronContainer() {
+  return (
+    <Jumbotron.Container>
+      <Jumbotron.Title>
+        <h1>Hello Netflix</h1>
+      </Jumbotron.Title>
+      <Jumbotron.SubTitle>
+        <h1>Hello Netflix</h1>
+      </Jumbotron.SubTitle>
+    </Jumbotron.Container>
+  );
+}
