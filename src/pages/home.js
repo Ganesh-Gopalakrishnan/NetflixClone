a5c40bc8594c3bcf170d18b1a13a730d3aaa40c3
Ganesh-Gopalakrishnan <88496221+Ganesh-Gopalakrishnan@users.noreply.github.com>
2021-11-28 19:20:15 +0530

    Basic components

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Jumbotron from "../components/jumbotron";
+
+export default function Home() {
+  return (
+    <Jumbotron.Container>
+      <h1>Hello Netflix</h1>
+    </Jumbotron.Container>
+  );
+}
